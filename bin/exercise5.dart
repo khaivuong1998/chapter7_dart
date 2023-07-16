cbdfd79e80d11265f413e913d4a80a783d358481
khaivuong1998 <khaivuong311098@gmail.com>
2023-07-16 09:54:04 +0700

    first commit

--- bin/exercise5.dart
+++ bin/exercise5.dart
@@ -3,4 +3,10 @@ import 'dart:io';
 void main() {
   print('mời nhập mã sinh viên');
   String str = stdin.readLineSync() ?? "";
+  final NUMERIC = RegExp(r'B170[1-9][1-9][1-9][1-9]+$');
+  if (str.isNotEmpty && NUMERIC.hasMatch(str)) {
+    print('mã bạn nhập chính xác ${str}');
+  } else {
+    print('mã sai');
+  }
 }
